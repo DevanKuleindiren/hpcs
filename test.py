9231830f606db009f8185362f81252a115e93d59
Devan Kuleindiren <d.kuleindiren@gmail.com>
2017-02-04 00:04:59 +0000

    Fix test file.

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ def main(_):
     print "I WORKED!"
     print 'Number of arguments:', len(sys.argv), 'arguments.'
     print 'Argument List:', str(sys.argv)
-    with open("/home/dk503/proj/test.txt"), "wb") as f:
+    with open("/home/dk503/proj/test.txt", "wb") as f:
         f.write("Blah blah blah.")
 
 if __name__ == "__main__":
