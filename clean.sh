1f0ff7602de7fcf29ca02ab408a86ea89c7d7bf9
Devan Kuleindiren <d.kuleindiren@gmail.com>
2017-02-06 20:22:22 +0000

    Add prompt to avoid accidental deletion of training logs.

--- clean.sh
+++ clean.sh
@@ -1,3 +1,8 @@
-rm *.out
-rm *.log
-rm *.err
+read -p "Are you sure you want to delete log files? (Remember to save the perplexity logs from training!) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	rm *.out
+	rm *.log
+	rm *.err
+fi
